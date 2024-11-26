d642438661e48e330639dfa2df13abe531d8f9c5
pyshkovni <guess.the.tutor@gmail.com>
2024-11-26 10:29:59 +0300

    потренировались с os

--- main.py
+++ main.py
@@ -3,10 +3,17 @@
 # импорты из модуля
 # from functions import func1, func2
 from test_python_docx import python_docx_tests
+from test_pandas import pandas_tests
+from test_os import os_tests
 
 # функция запуска импортированных функций
 def main():
+    print("----- Тесты python-docs -----")
     python_docx_tests("data/Заголовок.docx")
+    print("----- Тесты pandas -----")
+    pandas_tests("data/ab.xlsx")
+    print("----- Тесты os -----")
+    os_tests()
 
 # инициализационный скрипт
 if __name__ == '__main__':
